9ccbc6bdfa01a9519b46c77e86ff35eb24573368
anonboy96 <118814879+anonboy96@users.noreply.github.com>
2023-07-13 20:06:40 +0530

    Update index.py

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ from movies_scraper import search_movies, get_movie
 
 
 TOKEN = os.getenv("TOKEN")
-URL = "https://uflixy.vercel.app"
+URL = os.getenv("https://uflixy.vercel.app")
 bot = Bot(TOKEN)
 
 
